9c3ebea1efe9670c711f2251f23ab3b6628c7941
Галима <@DESKTOP-PGIEF0N>
2023-04-24 08:11:33 +0300

    Network Files DONE

--- Files/main.cpp
+++ Files/main.cpp
@@ -6,8 +6,7 @@ using namespace std;
 
 //#define WRITE_TO_FILE
 //#define READ_FROM_FILE
-
-
+#define WALL_FILES
 
 void main()
 {
@@ -26,10 +25,10 @@ void main()
 	//4) Потоки обязательно нужно закрывать! 
 	//Нельзя оставлять потоки открытыми, это может привести к непредвиденным последствиям!!!
 	fout.close();
-
 	system("notepad File.txt");
 
 #endif // WRITE_TO_FILE
+
 #ifdef READ_FROM_FILE
 	//0) Обьявляем буфер чтения:
 	const int SIZE = 256;
@@ -59,32 +58,54 @@ void main()
 #endif // READ_FROM_FILE
 
 
+#ifdef WALL_FILES
 	const int NUMBER_SIZE = 20;
 	char room_number[NUMBER_SIZE];
 	char sz_source_filename[FILENAME_MAX] = {};
-	char sz_ready_filename[FILENAME_MAX] = {};
+	char sz_wal_filename[FILENAME_MAX] = {};
+	char sz_dhcpd_filename[FILENAME_MAX] = {}; 
+
 	cout << "Введите номер аудитории: "; cin >> room_number;
 	strcat(sz_source_filename, room_number);
-	strcat(sz_ready_filename, room_number);
+	strcat(sz_wal_filename, room_number);
+	strcat(sz_dhcpd_filename, room_number);
 
 	strcat(sz_source_filename, " RAW.txt");
-	strcat(sz_ready_filename, " ready.txt");
+	strcat(sz_wal_filename, " ready.txt");
+	strcat(sz_dhcpd_filename, " .dhcpd");
 
 	const int IP_BUFFER_SIZE = 16;
 	const int MAC_BUFFER_SIZE = 18;
 	char sz_ip_buffer[IP_BUFFER_SIZE] = {};
 	char sz_mac_buffer[MAC_BUFFER_SIZE] = {};
+	
 
+	std::ofstream fout_dhcpd(sz_dhcpd_filename);
+	std::ofstream fout_wal(sz_wal_filename);
 	std::ifstream fin(sz_source_filename);
-	std::ofstream fout(sz_ready_filename);
-	if (fin.is_open())
+	if(fin.is_open())
 	{
-		while (!fin.eof())
+		for (int i=0; !fin.eof(); i++)
 		{
 			//TODO: exchange
 			fin >> sz_ip_buffer >> sz_mac_buffer;
-			fout << sz_mac_buffer << "\t" <<sz_ip_buffer << endl;
-			cout << sz_mac_buffer << "\t" <<sz_ip_buffer << endl;
+			if (strlen(sz_ip_buffer) == 0 && strlen(sz_mac_buffer) == 0)continue;
+			fout_wal << sz_mac_buffer << "\t" << sz_ip_buffer << endl;
+			//cout << sz_mac_buffer << "\t" << sz_ip_buffer << endl;
+			for (int i = 0; sz_mac_buffer[i]; i++) if (sz_mac_buffer[i] == '-')sz_mac_buffer[i] = ':';
+			cout << "host-" << room_number << "-" << i << ":\n";
+			cout << "{\n";
+			cout << "\thardware ethernet\t" << sz_mac_buffer << ";\n";
+			cout << "\tfixed-adress\t\t" << sz_ip_buffer << ";\n";
+			cout << "}\n";
+			cout << endl;
+
+			fout_dhcpd << "host-" << room_number << "-" << i << ":\n";
+			fout_dhcpd << "{\n";
+			fout_dhcpd << "\thardware ethernet\t" << sz_mac_buffer << ";\n";
+			fout_dhcpd << "\tfixed-adress\t\t" << sz_ip_buffer << ";\n";
+			fout_dhcpd << "}\n";
+			fout_dhcpd << endl;
 		}
 		fin.close();
 	}
@@ -92,11 +113,17 @@ void main()
 	{
 		std::cerr << "Error: file not found" << endl;
 	}
-	fout.close();
+	fout_dhcpd.close();
+	fout_wal.close();
 
-	char sz_command[FILENAME_MAX] = "notepad ";
-	strcat(sz_command, sz_ready_filename);
+	char sz_command_wal[FILENAME_MAX] = "start notepad ";
+	char sz_command_dhcpd[FILENAME_MAX] = "start notepad ";
+	strcat(sz_command_wal, sz_wal_filename);
+	strcat(sz_command_dhcpd, sz_dhcpd_filename);
 
-	system(sz_command);
+	system(sz_command_wal);
+	system(sz_command_dhcpd);
+#endif // WALL_FILES
+
 
 }
